9bea42d84eac7d0bf736f8a3f59767a42323c4d3
wint0074 <wint0074@stthomas.edu>
2019-09-27 20:09:22 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = "sixth-arbor-252201"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "dock-tut" {
+  name         = "dock-tut"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
